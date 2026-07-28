c6c53d9ef4645dad9f367c606049c4845cebe681
abdullah <abdullaharifsalimee@gmail.com>
2026-07-28 16:16:11 +0500

    ok

--- components/WorkCarousel.tsx
+++ components/WorkCarousel.tsx
@@ -142,7 +142,7 @@ const CARDS: CardItem[] = [
     desc: "Continental is a collaborative project built with Next.js, CSS, and JavaScript. It provides a comprehensive solution for managing educational institutions, including student enrollment, course management, attendance tracking, and performance analysis. The system is designed to streamline administrative tasks and enhance the overall learning experience.",
     skills: ["Next.js", "Tailwind CSS", "JavaScript"],
     Githuburl: "https://github.com/AbdullahSalimee/Continental",
-    url: "https://continental.vercel.app/",
+  
   },
 
   // ── JOBS ──────────────────────────────────────────────────────────────────
